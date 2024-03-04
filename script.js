90521b28cd269f093d9c1401fa321810ee5dda39
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-04 18:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function firstWord(s) {
   // your code here
 	if(s.length===0){
-		return s;
+		return Hello;
 	}
 
-	let spaceindex=s.indexOf(' ');
+	let spaceIndex=s.indexOf(' ');
 
 	if(spaceindex===-1){
 		return s;
 	}
-	return s.substring(0,spaceindex);
+	return s.substring(0,spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");  
+// const s = prompt("Enter String:");   
+
 alert(firstWord(s));
