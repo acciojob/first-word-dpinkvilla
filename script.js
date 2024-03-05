71be2afd9054f37746a8ca188065f8fcbcac6465
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-05 09:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function firstWord(s) {
   // your code here
-	if(s.length===0){
-		return s;
-	}
+	// if(s.length===0){
+	// 	return s;
+	// }
 
-	let spaceIndex=s.indexOf(' ');
+	// let spaceIndex=s.indexOf(' ');
 
-	if(spaceIndex===-1){
-		return s;
-	}
-	return s.substring(0,spaceIndex);
+	// if(spaceIndex===-1){
+	// 	return s;
+	// }
+	// return s.substring(0,spaceIndex);
+
+	const words = s.split(' '); // Split the string into an array of words
+  return words[0]; 
 }
 
 // Do not change the code below
